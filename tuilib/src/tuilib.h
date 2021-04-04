355ea9fff4c5e8834a8b20dca39ae7258c3cfbe8
Timofey Bakunin <bakunin.t@protonmail.com>
2021-04-04 03:28:24 +0000

    move ansi color defs to header in utilib

--- tuilib/src/tuilib.h
+++ tuilib/src/tuilib.h
@@ -1,4 +1,14 @@
 #include <stddef.h>
+
+#ifndef ANSI_COLORS
+#define ANSI_COLORS
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#endif
+
 #ifndef TUILIB_H
 #define TUILIB_H
 
