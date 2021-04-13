28e45e08526aaa0a8174e003681bae9d3b86b69f
Timofey Bakunin <bakunin.t@protonmail.com>
2021-04-13 11:59:08 +0300

    fix test, fix gitignore; seems to work lol

--- 7_lab/src/test.c
+++ 7_lab/src/test.c
@@ -3,23 +3,24 @@
 #include <stdio.h>
 
 #include "llrb.h"
+#include "utils.h"
 
 int main() {
-  struct LLRBNode *root = NULL;
-
   srandom((unsigned int)time(NULL));
-
+   
+  struct LLRB* tree = mknew(struct LLRB);
+  tree->root = NULL;
   for (t_key i = 0; i < 1000; ++i) {
-    tree_insert(&root, rand()%10000, i);
+    tree_insert(tree, rand()%10000, i);
   }
 
-  tree_save(root, 1);
+  tree_save(tree->root, 1);
 
   /*
   for (t_key i = 0; i < 50; ++i) {
   	printf("key: %lu, value: %lu\n",  i, tree_get(root, i));
   }
   */
-  tree_free(root);
+  tree_free(tree->root);
   return (0);
 }
